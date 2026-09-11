f5c03dbb8f092bc1d81c604ff177ac6504a4c486
kiddbg <himynameisvictor83@icloud.com>
2026-09-11 04:47:51 -0400

    Connect iOS app to production BGTalk backend by default

--- BGTalkApp/TranslationServiceFactory.swift
+++ BGTalkApp/TranslationServiceFactory.swift
@@ -1,8 +1,14 @@
 import Foundation
 
 struct TranslationServiceFactory {
+    private static let defaultEndpoint = "https://bgtalk-backend-production.up.railway.app/translate"
+
     static func makeService() -> any TranslationService {
-        let endpointString = UserDefaults.standard.string(forKey: "translationEndpoint") ?? ""
+        let configuredEndpoint = UserDefaults.standard.string(forKey: "translationEndpoint") ?? ""
+        let endpointString = configuredEndpoint.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            ? defaultEndpoint
+            : configuredEndpoint
+
         if let endpoint = URL(string: endpointString), endpoint.scheme != nil, endpoint.host != nil {
             return RemoteTranslationService(endpoint: endpoint)
         }
