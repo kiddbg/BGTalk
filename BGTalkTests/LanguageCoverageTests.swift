c0283232d3120840403895bc59333f4b83f794f0
kiddbg <himynameisvictor83@icloud.com>
2026-09-11 12:53:42 -0400

    Add language coverage tests

---
+++ BGTalkTests/LanguageCoverageTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import BGTalk
+
+final class LanguageCoverageTests: XCTestCase {
+    func testAllSupportedLanguagesHaveSpeechLocales() {
+        for language in AppLanguage.allCases {
+            XCTAssertFalse(language.speechLocale.isEmpty)
+        }
+    }
+
+    func testSupportedLanguageCodes() {
+        XCTAssertEqual(Set(AppLanguage.allCases.map(\.rawValue)), ["bg", "en", "es"])
+    }
+}
